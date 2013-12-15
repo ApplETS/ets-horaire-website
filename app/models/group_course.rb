2ed9c2bb3a42badb191ecf8d5ea18a8e36a8ee3c
Krystian Czesak <krystianczesak@gmail.com>
2013-12-15 17:33:30 -0500

    Corrected GroupCourse model

--- app/models/group_course.rb
+++ app/models/group_course.rb
@@ -10,19 +10,13 @@ class GroupCourse < Group
   end
 
   def conflicts?(group)
-    same_course_name?(group) || periods_conflict?(group)
+    same_course_name?(group) || super(group)
   end
 
   private
 
   def same_course_name?(group)
     @course_name == group.course_name
-  end
-
-  def periods_conflict?(group)
-    periods.any? do |period|
-      group.periods.any? { |comparable_period| period.conflicts?(comparable_period) }
-    end
   end
 
 end
