17bd76c77d81963084d11587a5ef30f39eb29c1d
Krystian <krystianczesak@gmail.com>
2013-12-25 00:27:14 -0500

    Bachelor selection has now only 1 step

--- app/controllers/select_bachelor_controller.rb
+++ app/controllers/select_bachelor_controller.rb
@@ -1,28 +1,5 @@
-# encoding: UTF-8
-
 class SelectBachelorController < ApplicationController
-  before_filter :ensure_year_period_selected, only: :choose
-
   def index
-    render_trimesters
-  end
-
-  def choose
-    trimester_slug, bachelor_slug = params[:year_period].split('-')
-    redirect_to select_courses_path(trimestre: trimester_slug, baccalaureat: bachelor_slug)
-  end
-
-  private
-
-  def ensure_year_period_selected
-    return if params.has_key?(:year_period)
-
-    flash[:notice] = 'Veuillez choisir un baccalauréat.'
-    render_trimesters
-  end
-
-  def render_trimesters
     @trimesters = TrimesterDatabase.instance.sorted
-    render 'index'
   end
 end
