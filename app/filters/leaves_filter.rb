71c1a148c1534133aeeb4965acfe87a8452adb35
Krystian <krystianczesak@gmail.com>
2014-03-06 09:54:59 -0500

    Changed some method names

--- app/filters/leaves_filter.rb
+++ app/filters/leaves_filter.rb
@@ -1,10 +1,8 @@
 class LeavesFilter
-  class << self
-    def scan(group, leaves)
-      group.periods.all? do |period|
-        leaves.none? do |leave|
-          leave.conflicts?(period)
-        end
+  def self.valid?(group, leaves)
+    group.periods.all? do |period|
+      leaves.none? do |leave|
+        leave.conflicts?(period)
       end
     end
   end
