ce868261d85d27eeb1ed41181a92912248bb2ba8
Krystian <krystianczesak@gmail.com>
2014-03-08 14:00:54 -0500

    Adding leave concept in models

--- app/builders/leave_builder.rb
+++ app/builders/leave_builder.rb
@@ -3,6 +3,6 @@ class LeaveBuilder
     weekday = Weekday.new(leave['weekday'].to_i)
     start_time = WeekdayTime.new(weekday, leave['from-time'].to_i, 0)
     end_time = WeekdayTime.new(weekday, leave['to-time'].to_i, 0)
-    Period.new weekday, 'Leave', start_time, end_time
+    Leave.new weekday, 'Leave', start_time, end_time
   end
 end
