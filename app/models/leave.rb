f3b07eedac6f482b13d32332de97805d739bbf6f
Krystian <krystianczesak@gmail.com>
2014-06-12 22:40:18 -0400

    Extracting view data to leave model

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -1,4 +1,7 @@
 class Leave < Period
+  FROM_TIME_RANGE = (8..23)
+  TO_TIME_RANGE = (9..24)
+
   def initialize(weekday, start_time, end_time)
     super weekday, 'Leave', start_time, end_time
   end
