62a0e8def76e3cfab64e5c0897cc723f99ce0995
Krystian <krystianczesak@gmail.com>
2014-07-26 12:30:47 -0400

    Version header correction on mobile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module EtsHoraire
       attr_accessor :version
     end
 
+    self.version = 'local'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
