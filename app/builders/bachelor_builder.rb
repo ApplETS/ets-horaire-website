5726fdde33848e634591652cdf174252a1dea6de
Krystian <krystianczesak@gmail.com>
2014-08-03 22:56:24 -0400

    Simplifying builders

--- app/builders/bachelor_builder.rb
+++ app/builders/bachelor_builder.rb
@@ -3,7 +3,7 @@
 class BachelorBuilder
   class << self
     def build(file_struct, courses_struct)
-      courses = courses_struct.collect { |course_struct| CourseBuilder.build course_struct }
+      courses = courses_struct.collect { |course_struct| build_course(course_struct) }
       CourseUtils.cleanup! courses
 
       Bachelor.new bachelor_name_from(file_struct), file_struct.slug, courses
@@ -15,6 +15,16 @@ class BachelorBuilder
       Bachelor::NAMES.fetch(file_struct.slug)
     rescue KeyError
       raise "Missing bachelor slug '#{file_struct.slug}' in BachelorBuilder."
+    end
+
+    def build_course(course_struct)
+      groups = course_struct.groups.collect { |gs| build_group(gs) }
+      Course.new(course_struct.name, groups)
+    end
+
+    def build_group(group_struct)
+      periods = group_struct.periods.collect { |ps| PeriodBuilder.build(ps) }
+      Group.new(group_struct.nb, periods)
     end
   end
 end
