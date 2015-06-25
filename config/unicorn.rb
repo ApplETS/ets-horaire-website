54579834b641805a3a0a4c30c50fe9d5b37b016b
Annie Caron <contact@anniecaron.ca>
2015-06-25 16:01:57 -0400

    Début du designÈ

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -34,9 +34,9 @@ before_fork do |server, worker|
 	old_pid = "#{server.config[:pid]}.oldbin"
 	if File.exists?(old_pid) && server.pid != old_pid
 		begin
-			Process.kill("QUIT", File.read(old_pid).to_i)
+			Process.kill("-9", File.read(old_pid).to_i)
 		rescue Errno::ENOENT, Errno::ESRCH
-		# someone else did our job for us
+			# someone else did our job for us
 		end
 	end
 end
